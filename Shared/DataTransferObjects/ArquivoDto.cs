ba124fb61b576be2151443d8dab7d39e8b7e8c5a
joaobiener <joaobiener@hotmail.com>
2023-02-17 11:02:18 -0300

    Uploads Imagens

--- Shared/DataTransferObjects/ArquivoDto.cs
+++ Shared/DataTransferObjects/ArquivoDto.cs
@@ -3,11 +3,15 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Shared.DataTransferObjects;
 
-public record ArquivoDto(  Guid Id,
-								string? Nome,
-								string? Tipo,
-								byte[] DataFiles,
-								string? CriadoPor,
-								DateTime? CriadoEm);
+public record ArquivoDto { 
+	
+	public Guid Id { get; set; }
+	public string? Nome  { get; set; }
+	public string? Tipo { get; set; }
+	public byte[] DataFiles { get; set; }
+	public string? CriadoPor { get; set; }
+
+	public DateTime? CriadoEm{ get; set; }
 
 
+}
