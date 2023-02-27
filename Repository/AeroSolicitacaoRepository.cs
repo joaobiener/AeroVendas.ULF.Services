96c3197276ffd033e72e29dd010965570097cd7f
joaobiener <joaobiener@hotmail.com>
2023-02-27 17:44:23 -0300

    Ajustes

--- Repository/AeroSolicitacaoRepository.cs
+++ Repository/AeroSolicitacaoRepository.cs
@@ -18,13 +18,23 @@ internal sealed class AeroSolicitacaoRepository : RepositoryBase<AeroSolicitacao
 
 	public async Task<PagedList<AeroSolicitacaoEmail>> GetAllAeroSolicitacaAsync(ViewAeroVendasParameters viewAeroVendasParameters, bool trackChanges)
 	{
-		throw new NotImplementedException();
+		var aeroSolicitacao = await FindByCondition(x =>
+						   x.Id != null
+						  , trackChanges)
+						  .Search(viewAeroVendasParameters.SearchTerm)
+						.Sort(viewAeroVendasParameters.OrderBy)
+						.ToListAsync();
+
+		return PagedList<AeroSolicitacaoEmail>
+			   .ToPagedList(aeroSolicitacao,
+							viewAeroVendasParameters.PageNumber,
+							viewAeroVendasParameters.PageSize);
 	}
 
-	public async Task<AeroSolicitacaoEmail> GetAeroSolicitacaAsync(Guid aeroSolicitacaoId, bool trackChanges)
-	{
-		throw new NotImplementedException();
-	}
+	public async Task<AeroSolicitacaoEmail> GetAeroSolicitacaAsync(Guid aeroSolicitacaoId, bool trackChanges) =>
+			await FindByCondition(c => c.Id.Equals(aeroSolicitacaoId), trackChanges)
+			.SingleOrDefaultAsync();
+
 
 	public void CreateAeroSolicitacao(AeroSolicitacaoEmailForCreationDto aeroSolicitacao)
 	{
